5a4e7f2044c262f890305088d64d521fd857391c
MishaelRatemo <mishael.ratemo@student.moringaschool.com>
2022-02-21 19:57:09 +0300

    templates inheritance

--- news/views.py
+++ news/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.http import HttpResponse,Http404
 import datetime as dt
 
@@ -8,30 +8,11 @@ def welcome(request):
 
 def news_of_day(request):
     date = dt.date.today()
-    day= convert_dates(date)
-    html = f'''
-        <html>
-            <body>
-                <h1>news for {day} {date.day}-{date.month}-{date.year}</h1>
-            </body>
-        </html>
-            '''
-    return HttpResponse(html)
-def convert_dates(dates):
     
-    # Function that gets the weekday number for the date.
-    day_number = dt.date.weekday(dates)
-
-    days = ['Monday','Tuesday','Wednesday','Thursday','Friday','Saturday',"Sunday"]
-
-    # Returning the actual day of the week
-    day = days[day_number]
-    return day
+    return render(request, 'all-news/today-news.html', {"date":date})
 
 def past_days_news(request,past_date):
-        # Converts data from the string Url
-    date = dt.datetime.strptime(past_date,'%Y-%m-%d').date()
-
+   
     try:
         # Converts data from the string Url
         date = dt.datetime.strptime(past_date,'%Y-%m-%d').date()
@@ -39,13 +20,8 @@ def past_days_news(request,past_date):
     except ValueError:
         # Raise 404 error when ValueError is thrown
         raise Http404()
+        assert False
+    if date == dt.date.today():
+        return redirect(news_of_day)
     
-    day = convert_dates(date)
-    html = f'''
-        <html>
-            <body>
-                <h1>News for {day} {date.day}-{date.month}-{date.year}</h1>
-            </body>
-        </html>
-            '''
-    return HttpResponse(html)
+    return render(request, 'all-news/past-news.html',{"date": date})
