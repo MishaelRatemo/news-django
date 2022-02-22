9c2a01055f5867b31a5c82ca388b376edcc2d02f
MishaelRatemo <mishael.ratemo@student.moringaschool.com>
2022-02-22 05:42:44 +0300

    Added a Timestamp

--- news/models.py
+++ news/models.py
@@ -18,10 +18,11 @@ class tags(models.Model):
     def __str__(self):
         return self.name
 
-#One to many relations
+
 class Article(models.Model):
     title = models.CharField(max_length =60)
     post = models.TextField()
     editor = models.ForeignKey(Editor)
     
     tags = models.ManyToManyField(tags)
+    pub_date = models.DateTimeField(auto_now_add=True)
