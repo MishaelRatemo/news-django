7c3c67ce853048b7450dc8b5705884f775bd985f
MishaelRatemo <mishael.ratemo@student.moringaschool.com>
2022-02-22 06:09:00 +0300

    created test case and instance for editor

--- news/tests.py
+++ news/tests.py
@@ -1,3 +1,12 @@
 from django.test import TestCase
+from .models import Editor,Article,tags
 
-# Create your tests here.
+class EditorTestClass(TestCase):
+    
+    # Set up method
+    def setUp(self):
+        self.widen= Editor(first_name = 'Widen', last_name ='Ratemo', email ='widen@gmail.com')
+    
+    # Testing  instance to confirm that the object is being instantiated correctly.
+    def test_instance(self):
+        self.assertTrue(isinstance(self.widen,Editor))   
