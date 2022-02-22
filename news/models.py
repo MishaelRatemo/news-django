29f111f1491c706a1afe4535eeb20432f514c463
MishaelRatemo <mishael.ratemo@student.moringaschool.com>
2022-02-22 04:53:34 +0300

    created model-Editor

--- news/models.py
+++ news/models.py
@@ -1,3 +1,9 @@
+import email
 from django.db import models
 
-# Create your models here.
+class Editor(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    email = models.EmailField()
+    
+    
