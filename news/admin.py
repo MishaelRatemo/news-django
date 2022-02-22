159a6d2626f940223f8e769d18fb5d072e2c1939
MishaelRatemo <mishael.ratemo@student.moringaschool.com>
2022-02-22 08:57:24 +0300

    updates

--- news/admin.py
+++ news/admin.py
@@ -2,6 +2,9 @@ from django.contrib import admin
 from .models import Editor,Article,tags
 
 # Register your models here.
+class ArticleAdmin(admin.ModelAdmin):#inherits from the ModelAdmin class
+    filter_horizontal =('tags',) #allows ordering of many to many fields and pass in the tags article field
+
 admin.site.register(Editor)
-admin.site.register(Article)
+admin.site.register(Article,ArticleAdmin)
 admin.site.register(tags)
